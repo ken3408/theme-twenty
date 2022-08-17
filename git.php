3efb9384dcf986499fd74d80d8425c38113f38e5
ken3408 <60221904+ken3408@users.noreply.github.com>
2022-08-18 01:30:55 +0900

    コード全書き換え

--- git.php
+++ git.php
@@ -1,1 +1,15 @@
-push test!
+<?php
+$repository = "ファイルやディレクトリの状態を記録する場所";
+?>
+<!DOCTYPE html>
+<html lang='ja'>
+<head>
+<meta charset='UTF-8'>
+<title>課題</title>
+</head>
+<body>
+<?php
+echo $repository;
+?>
+</body>
+</html>
