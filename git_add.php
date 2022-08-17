d9d165fccc030ba338241aea603a006c62ca8ed9
ken3408 <60221904+ken3408@users.noreply.github.com>
2022-08-18 01:45:49 +0900

    git_add.phpを作成

---
+++ git_add.php
@@ -0,0 +1,15 @@
+<?php
+$add = "修正したファイルをインデックス（ステージング）に反映";
+?>
+<!DOCTYPE html>
+<html lang='ja'>
+<head>
+<meta charset='UTF-8'>
+<title>課題</title>
+</head>
+<body>
+<?php
+echo $add;
+?>
+</body>
+</html>
